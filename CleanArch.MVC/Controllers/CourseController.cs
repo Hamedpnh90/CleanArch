8bd3861b243d15aebc0a8fd939ce250118404ce9
Hamedpnh90 <103711949+Hamedpnh90@users.noreply.github.com>
2023-01-01 16:44:21 +0330

    Create CourseController

---
+++ CleanArch.MVC/Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        ICourseServices _courseServices;
+        public CourseController(ICourseServices courseServices)
+        {
+            _courseServices = courseServices;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel Model = _courseServices.GetCourses();
+            return View(Model);
+        }
+    }
+}
